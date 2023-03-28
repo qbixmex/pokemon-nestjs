ede4954d16bcb4108f42addf31711cbaf39306aa
Daniel González Briseño <qbixmex@gmail.com>
2023-03-28 05:55:26 -0600

    mongo-db-config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(process.env.MONGO),
     PokemonModule
   ],
   controllers: [],
